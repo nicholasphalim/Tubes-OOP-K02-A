9f7a5e8a6ec6f0ba3a54548aa628d915630ed4ce
12ahmatp <rahmatpuji01@gmail.com>
2025-12-11 14:13:49 +0700

    Add GUI message for trashing action

--- src/main/java/station/TrashStation.java
+++ src/main/java/station/TrashStation.java
@@ -1,47 +1,55 @@
 package station;
 
 import item.*;
-import entity.Chef;
+import entity.*;
 import inventory.Plate;
 import main.GamePanel;
-import object.SuperObject;
-import item.Dish;
-import ingredient.Ingredient;
+import object.*;
+import ingredient.*;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
 
 public class TrashStation extends Station {
     
-    public TrashStation() {
-        //super gp?
+    public TrashStation(GamePanel gp) {
+        super(gp);
     }
 
     @Override
     public boolean canAccept(SuperObject item) {
-        //menerima dish atau ingredient
-        boolean isAcceptable = (item instanceof Dish) || (item instanceof Ingredient);
-        return isAcceptable;
+        //accept dish or ingredient
+        return (item instanceof Dish) || (item instanceof Ingredient);
     }
 
     @Override
     public void interact(Chef chef) {
         if (chef.getInventory() == null) {
-            return; //nothing to trash
+            chef.gp.ui.showMessage("Nothing to trash!");
+            return;
         } else {
             if (this.canAccept(chef.getInventory())) {
                 this.placeItem(chef.getInventory());
+                chef.gp.ui.showMessage("Item trashed!");
                 chef.setInventory(null);
             } else if (chef.getInventory() instanceof Plate) {
                 Plate plate = (Plate) chef.getInventory();
                 if (plate.getContents() != null) {
                     //this.placeItem(plate.getContents());
+                    chef.gp.ui.showMessage("Plate contents trashed!");
                     plate.clearContents();
                 }
+            } else {
+                chef.gp.ui.showMessage("Cannot trash this item!");
             }
         }
-        this.itemOnStation = null; //langsung hilang setelah dibuang
+        this.itemOnStation = null; //disappear the trashed item
     }
 
     @Override
     public Item takeItem() {
-        return null; //tidak bisa diambil kembali
+        gp.ui.showMessage("Cannot take items from Trash Station!");
+        return null;
     }
 }
