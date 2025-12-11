a8688867ed063c88ccf0c0eb792345015a7621da
12ahmatp <rahmatpuji01@gmail.com>
2025-12-11 16:43:41 +0700

    Add fields penalty and orderlist

--- src/main/java/station/ServingCounter.java
+++ src/main/java/station/ServingCounter.java
@@ -1,22 +1,31 @@
 package station;
 
 import inventory.Plate;
-import entity.Chef;
+import entity.*;
 import item.Dish;
 import order.*;
 import main.GamePanel;
 import object.SuperObject;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+
 public class ServingCounter extends Station {
-    public ServingCounter() {
-        //super gp?
+    private static int penalty = 20;
+    private OrderList orderList;
+
+    //instantiate with order list
+    public ServingCounter(GamePanel gp, OrderList orderList) {
+        super(gp);
+        this.orderList = orderList;
     }
 
     @Override
     //covariant parameter type
     public boolean canAccept(SuperObject plate) {
+        //accept only plates with contents
         return this.itemOnStation == null && ((Plate) plate).getContents() != null;
-        //menerima Plate tak kosong
     }
 
     @Override
@@ -27,17 +36,26 @@ public class ServingCounter extends Station {
                 this.placeItem(chef.getInventory());
                 chef.setInventory(null);
                 this.serve(plate);
+            } else {
+                gp.ui.showMessage("Cannot serve empty plate!");
             }
         }
     }
 
     @Override
     public Plate takeItem() {
-        return null; //tidak bisa diambil setelah drop
+        gp.ui.showMessage("Cannot take items from Serving Counter!");
+        return null;
     }
 
-    public void serve(Plate plate) { //berhubungan dengan OrderList
-        return; //implementasi serve ke customer
-        //gimana ngasih penalti kalau tidak sesuai order
+    public void serve(Plate plate) {
+        boolean correct = orderList.validateOrder(plate.getContents());
+        if (correct) {
+            gp.ui.showMessage("Served dish correctly! +" + orderList.getOrders().get(0).getReward() + " points.");
+            GamePanel.addScore(orderList.getOrders().get(0).getReward());
+        } else {
+            gp.ui.showMessage("Served dish incorrectly! -" + penalty + " points.");
+            GamePanel.addScore(-penalty);
+        }
     }
 }
